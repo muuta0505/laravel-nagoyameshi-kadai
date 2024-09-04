1de4a643a3a0502796b1862f6cf4cb0c0d1ba54a
muuta0505 <kouhei9582@icloud.com>
2024-09-04 19:17:40 +0900

    2回目のコミット

---
+++ database/migrations/2024_09_02_195906_add_kana_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('kana')->after('name'); // 'kana'列を追加します
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('kana'); // 必要に応じて列を削除します
+        });
+    }
+};
