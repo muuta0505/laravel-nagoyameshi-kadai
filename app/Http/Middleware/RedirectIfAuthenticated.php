b9da93c7ddb62302c055b04eb67dfa21ac2786c8
muuta0505 <kouhei9582@icloud.com>
2024-09-17 20:33:30 +0900

    3回目のコミット

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,8 +20,8 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if ($guard === 'admin') {
+                return redirect(RouteServiceProvider::ADMIN_HOME);
             }
         }
 
