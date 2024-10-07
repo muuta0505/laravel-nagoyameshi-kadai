637fe85cd86e6a316e6569ff4f30d63f399e7267
muuta0505 <kouhei9582@icloud.com>
2024-10-07 19:50:17 +0900

    修正

--- app/Models/category.php
+++ app/Models/category.php
@@ -16,9 +16,4 @@ class Category extends Model
         'created_at',
         'updated_at',
     ];
-
-    public function restaurants() {
-        return $this->belongsToMany(Restaurant::class)->withTimestamps();
-    }
 }
-
