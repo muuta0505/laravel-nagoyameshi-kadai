91d97101501248cec663881297b34e38bd62f034
muuta0505 <kouhei9582@icloud.com>
2024-10-15 18:30:33 +0900

    エラー修正

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
