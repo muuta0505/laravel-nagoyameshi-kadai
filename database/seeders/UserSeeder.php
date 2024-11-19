e5b5b13eca34d6a2bd664910f7c14e8e4dd6d3db
muuta0505 <kouhei9582@icloud.com>
2024-11-20 04:40:00 +0900

    提出

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(100)->create();
+        //User::factory()->count(100)->create();
+
+        $user = new User();
+        $user->name = "太郎";
+        $user->kana = "タロウ";
+        $user->email = 'tarou@example.com';
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make('password');
+        $user->postal_code = "0000000";
+        $user->address = "東京都";
+        $user->phone_number = "000-0000-0000";
+        $user->save();
     }
 }
